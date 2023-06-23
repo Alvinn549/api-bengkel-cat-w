35c3202df3eb5434eb1f8e7f126c2639b5cf2d85
Alvin Nuha M <alvinn549@gmail.com>
2023-06-23 15:34:09 +0700

    add route group

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User, Kendaraan } = require('../db/models');
 
-async function getAllUsers(req, res) {
+// * Get all user
+async function getAllUser(req, res) {
   try {
     const users = await User.findAll({
       attributes: [
@@ -22,7 +23,54 @@ async function getAllUsers(req, res) {
     res.status(500).json({ error: 'Internal server error' });
   }
 }
-async function getAllUsersWithKendaraan(req, res) {
+
+// * Get user by id
+async function getUserById(req, res) {
+  const userId = req.params.id; // Assuming the user ID is passed as a parameter in the URL
+
+  try {
+    const user = await User.findByPk(userId, {
+      attributes: [
+        'id',
+        'nama',
+        'no_telp',
+        'alamat',
+        'jenis_k',
+        'foto',
+        'email',
+        'role',
+        'device_id',
+      ],
+      include: {
+        model: Kendaraan,
+        as: 'Kendaraan',
+        attributes: ['id', 'no_plat', 'merek'],
+      },
+    });
+
+    if (!user) {
+      // User with the specified ID not found
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+// * Create new user
+async function storeUser(req, res) {}
+
+// * Update user
+async function updateUser(req, res) {}
+
+// * Delete user
+async function destroyUser(req, res) {}
+
+// ! Tes relasi
+async function tes(req, res) {
   try {
     const users = await User.findAll({
       attributes: [
@@ -51,6 +99,10 @@ async function getAllUsersWithKendaraan(req, res) {
 }
 
 module.exports = {
-  getAllUsers,
-  getAllUsersWithKendaraan,
+  getAllUser,
+  getUserById,
+  storeUser,
+  updateUser,
+  destroyUser,
+  tes,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,49 @@
+require('express-group-routes');
+
 const express = require('express');
-
 const { registerUser } = require('../controllers/registerController');
-
 const { Login, Logout } = require('../controllers/authentication');
-
 const { refreshToken } = require('../controllers/refreshTokenController');
-
 const {
-  getAllUsers,
-  getAllUsersWithKendaraan,
+  getAllUser,
+  getUserById,
+  storeUser,
+  updateUser,
+  destroyUser,
+  tes,
 } = require('../controllers/userController');
-
 const { verifyToken } = require('../middleware/VerifyToken');
 const { isAdmin } = require('../middleware/Admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World?');
 });
 
-router.post('/register', registerUser);
-router.post('/login', Login);
-router.get('/token', refreshToken);
-router.delete('/logout', Logout);
+router.group('/api', (router) => {
+  // ? "GET"  /api
+  router.get('/', (req, res) => {
+    res.send('Welcome to API');
+  });
 
-router.get('/users', verifyToken, isAdmin, getAllUsers);
-router.get('/tes', verifyToken, isAdmin, getAllUsersWithKendaraan);
+  router.post('/register', registerUser); // ?     User register -> "GET"    -> /api/register
+  router.post('/login', Login); // ?               Login         -> "POST"   -> /api/login
+  router.delete('/logout', Logout); // ?           Logout        -> "DELETE" -> /api/logout
+  router.get('/refresh-token', refreshToken); // ? Refresh token -> "GET"    -> /api/refresh-token
+
+  // * Route group /users
+  router.group('/users', (router) => {
+    router.get('/', verifyToken, isAdmin, getAllUser); // ?        Get all users  -> "GET"    -> /api/users
+    router.post('/', verifyToken, isAdmin, storeUser); // ?        Create user    -> "POST"   -> /api/users
+
+    router.get('/:id', verifyToken, isAdmin, getUserById); // ?    Get user by Id -> "GET"    -> /api/users/:id
+    router.put('/:id', verifyToken, isAdmin, updateUser); // ?     Update user    -> "PUT"    -> /api/users/:id
+    router.delete('/:id', verifyToken, isAdmin, destroyUser); // ? Delete user    -> "DELETE" -> /api/users/:id
+  });
+
+  // ? /api/tes
+  router.get('/tes', verifyToken, isAdmin, tes);
+});
 
 module.exports = router;
