acb61e06b027471cdb8e4439d8c5e79b8ad1645a
Alvin Nuha M <alvinn549@gmail.com>
2023-07-03 16:39:47 +0700

    Refactor code authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,39 +8,52 @@ async function login(req, res) {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(404).json({ message: 'Email not found!' });
+      return res
+        .status(404)
+        .json({ error: 'Email yang anda masukkan tidak ditemukan!' });
     }
 
     const match = await bcrypt.compare(password, user.password);
 
     if (!match) {
-      return res.status(400).json({ message: 'Wrong password!' });
+      return res
+        .status(400)
+        .json({ error: 'Password yang anda masukkan salah!' });
     }
 
     const { id: userId, nama, email: userEmail } = user;
 
-    const access_token = jwt.sign(
-      { userId, nama, email: userEmail },
-      process.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '1d',
-      }
-    );
+    const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+    const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
-    const refresh_token = jwt.sign(
-      { userId, nama, email: userEmail },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: '1d',
-      }
-    );
+    var access_token, refresh_token;
+
+    try {
+      access_token = generateToken(
+        { userId, nama, email: userEmail },
+        accessTokenSecret
+      );
+
+      refresh_token = generateToken(
+        { userId, nama, email: userEmail },
+        refreshTokenSecret
+      );
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ error: 'Failed to generate token', message: error.message });
+    }
 
     await User.update({ refresh_token }, { where: { id: userId } });
 
-    return res.cookie('refresh_token', refresh_token, {
+    res.cookie('refresh_token', refresh_token, {
       httpOnly: true,
-      maxAge: 24 * 60 * 1000,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      secure: false, // Set to true if running over HTTPS
     });
+
+    return res.json({ access_token });
   } catch (error) {
     console.error(error);
     return res
@@ -49,24 +62,33 @@ async function login(req, res) {
   }
 }
 
+function generateToken(payload, secret_key) {
+  const token = jwt.sign(payload, secret_key, {
+    expiresIn: '1d',
+  });
+  return token;
+}
+
 async function logout(req, res) {
   try {
     const refresh_token = req.cookies.refresh_token;
 
     if (!refresh_token) {
-      return res.sendStatus(204); // No Content
+      return res.sendStatus(204);
     }
 
     const user = await User.findOne({ where: { refresh_token } });
 
     if (!user) {
-      return res.sendStatus(403); // Forbidden
+      return res.sendStatus(403);
     }
 
     const userId = user.id;
     await User.update({ refresh_token: null }, { where: { id: userId } });
 
-    return res.clearCookie('refresh_token');
+    res.clearCookie('refresh_token');
+
+    return res.sendStatus(200);
   } catch (error) {
     console.error(error);
     return res
