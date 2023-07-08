bd564641bb152dafac98716c6ee0131975d322b5
Alvin Nuha M <alvinn549@gmail.com>
2023-07-08 11:29:00 +0700

    Add Email Verification

--- db/models/user.js
+++ db/models/user.js
@@ -12,6 +12,15 @@ module.exports = (sequelize, DataTypes) => {
         },
         as: 'kendaraan',
       });
+
+      // Has One
+      User.hasOne(models.UserActivation, {
+        foreignKey: {
+          name: 'user_id',
+          allowNull: false,
+        },
+        as: 'activation',
+      });
     }
   }
 
@@ -26,6 +35,7 @@ module.exports = (sequelize, DataTypes) => {
       role: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
+      isActive: DataTypes.BOOLEAN,
       refresh_token: { type: DataTypes.TEXT, allowNull: true },
       device_id: { type: DataTypes.TEXT, allowNull: true },
       createdAt: DataTypes.DATE,
---
+++ db/seeders/20230707234214-demo-user-activation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { faker } = require('@faker-js/faker/locale/id_ID');
+const { User } = require('../models');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = await User.findAll({
+      attributes: ['id', 'email'],
+    });
+
+    const fakeUserActivations = users.map((user) => {
+      return {
+        user_id: user.id,
+        email: user.email,
+        code: faker.string.alpha(6),
+        expireAt: faker.date.future(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    });
+
+    await queryInterface.bulkInsert('UserActivations', fakeUserActivations, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('UserActivations', null, {});
+  },
+};
