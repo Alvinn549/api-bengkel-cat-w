0fad3d7adf2e9b5b1c8b013c5c949c6550c252fe
Alvin Nuha M <alvinn549@gmail.com>
2023-09-10 12:47:42 +0700

    Add getProgresPerbaikanByPerbaikanId functionality

--- controllers/progresPerbaikanController.js
+++ controllers/progresPerbaikanController.js
@@ -53,6 +53,29 @@ async function getProgresPerbaikanById(req, res) {
   }
 }
 
+async function getProgresPerbaikanByPerbaikanId(req, res) {
+  try {
+    const { id: perbaikan_id } = req.params;
+    const progresPerbaikan = await ProgresPerbaikan.findAll({
+      where: { perbaikan_id: perbaikan_id },
+      order: [['createdAt', 'DESC']],
+    });
+
+    if (!progresPerbaikan) {
+      return res
+        .status(404)
+        .json({ message: 'Progres Perbaikan tidak ditemukan!' });
+    }
+
+    return res.status(200).json(progresPerbaikan);
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: 'Internal server error!', message: error.message });
+  }
+}
+
 async function storeProgresPerbaikan(req, res) {
   try {
     return res.status(200).json('storeProgresPerbaikan');
@@ -92,4 +115,5 @@ module.exports = {
   storeProgresPerbaikan,
   updateProgresPerbaikan,
   destroyProgresPerbaikan,
+  getProgresPerbaikanByPerbaikanId,
 };
