1fe89d2b0a05f00a20d484e972e03333966ba5b1
Alvin Nuha M <alvinn549@gmail.com>
2023-10-06 13:36:21 +0700

    Add transaksi validation schema

---
+++ validator/transaksiValidator.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const transaksiValidationSchema = Joi.object({
+  perbaikan_id: Joi.string().guid({ version: 'uuidv4' }).required(),
+  gross_amount: Joi.number().integer().required(),
+  tipe_bank: Joi.string().required(),
+  nama: Joi.string().required(),
+  no_telp: Joi.string().required(),
+  email: Joi.string().required(),
+});
+
+module.exports = { transaksiValidationSchema };
