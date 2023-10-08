6261ccdf767e9dd943f0a0c47b965b730e904d15
Alvin Nuha M <alvinn549@gmail.com>
2023-10-08 07:50:17 +0700

    Add retry logic if midtrans return error

--- controllers/transaksiController.js
+++ controllers/transaksiController.js
@@ -39,42 +39,57 @@ async function storeTransaksi(req, res) {
       return res.status(400).json({ message: errorMessage });
     }
 
-    var order_id = randomstring.generate(6);
+    var order_id = randomstring.generate(8);
+    let response_midtrans;
+    let retryCount = 3; // Set the maximum number of retries if
 
-    try {
-      const response_midtrans = await processTransaction(
-        order_id,
-        gross_amount,
-        tipe_bank,
-        nama,
-        no_telp,
-        email,
-        alamat
-      );
-      var status = response_midtrans.transaction_status;
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ error: 'Internal server error', message: error.message });
+    while (retryCount > 0) {
+      try {
+        response_midtrans = await processTransaction(
+          order_id,
+          gross_amount,
+          tipe_bank,
+          nama,
+          no_telp,
+          email,
+          alamat
+        );
+
+        var status = response_midtrans.transaction_status;
+
+        const newTransaksi = await Transaksi.create({
+          id: response_midtrans.transaction_id,
+          perbaikan_id,
+          order_id,
+          gross_amount,
+          tipe_bank,
+          status,
+          nama,
+          no_telp,
+          email,
+          response_midtrans: JSON.stringify(response_midtrans), // Store as a JSON string
+        });
+
+        return res.status(201).json({
+          message: 'Transaksi berhasil disimpan!',
+          newTransaksi,
+        });
+      } catch (error) {
+        console.error('Error occurred:', error.message);
+
+        retryCount--;
+
+        if (retryCount > 0) {
+          console.log(`Retrying... (${retryCount} retries remaining)`);
+
+          await new Promise((resolve) => setTimeout(resolve, 1000)); // 1 second delay
+        } else {
+          return res
+            .status(500)
+            .json({ error: 'Internal server error', message: error.message });
+        }
+      }
     }
-
-    const newTransaksi = Transaksi.create({
-      id: response_midtrans.transaction_id,
-      perbaikan_id,
-      order_id,
-      gross_amount,
-      tipe_bank,
-      status,
-      nama,
-      no_telp,
-      email,
-      response_midtrans,
-    });
-
-    return res.status(201).json({
-      message: 'Transaksi berhasil disimpan!',
-      newTransaksi,
-    });
   } catch (error) {
     console.error(error);
     return res
